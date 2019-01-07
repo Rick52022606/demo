1bb6f1ae87af5ca31c6e728364146ace73486194
Rick <rick32291057@yahoo.com.tw>
2019-01-07 15:49:51 +0800

    2.4

--- main.c
+++ main.c
@@ -18,6 +18,7 @@ int main()
     printf("Hello world!\n");
     printf("Hello world!\n");
     printf("Hello world!\n");
-    printf("Hello world!\n");
+    printf("     https://github.com/Rick52022606/demo.git\n");
+
     return 0;
 }
