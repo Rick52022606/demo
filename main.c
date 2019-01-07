bbc5c72134ae51d03e38b68dc60555b3e2059369
Rick <rick32291057@yahoo.com.tw>
2019-01-07 15:21:50 +0800

    2.1

--- main.c
+++ main.c
@@ -13,5 +13,7 @@ int main()
     printf("Hello world!\n");
     printf("Hello world!\n");
     printf("Hello world!\n");
+    printf("Hello world!\n");
+    printf("Hello world!\n");
     return 0;
 }
